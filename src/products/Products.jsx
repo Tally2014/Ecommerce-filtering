19c4e71f3888c4ffa5553f8eb523c3c4105b69d3
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 01:16:03 +0200

    24:40 - Products Section

--- src/products/Products.jsx
+++ src/products/Products.jsx
@@ -1,7 +1,38 @@
 import "./Products.css";
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
 
 export default function Products() {
   return (
-    <div>Products</div>
-  )
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/519MRhRKGFL._AC_UX575_.jpg"
+            alt="Shoe"
+            className="card-img"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>R1500</del> R999
+              </div>
+
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 }
