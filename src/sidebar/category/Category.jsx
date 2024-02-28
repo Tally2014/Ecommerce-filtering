b962000bb94b52d60c8c14fc5c3f2e271681bef3
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 22:17:37 +0200

    01:25:01 - Category Component Refactor

--- src/sidebar/category/Category.jsx
+++ src/sidebar/category/Category.jsx
@@ -1,20 +1,39 @@
 import "./Category.css";
 import Input from "../../components/Input";
 
-export default function Category() {
+export default function Category({ handleChange }) {
   return (
     <div>
       <h2 className="sidebar-title">Category</h2>
       <div className>
-        <label className="sidebar-label-container">
-          <input type="radio" value="" name="test" />
+        <label onChange={handleChange} className="sidebar-label-container">
+          <input type="radio" value="" name="category" />
           <span className="checkmark"></span>All
         </label>
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
+        <Input
+          handleChange={handleChange}
+          value="sneakers"
+          title="Sneakers"
+          name="category"
+        />
+        <Input
+          handleChange={handleChange}
+          value="flats"
+          title="Flats"
+          name="category"
+        />
+        <Input
+          handleChange={handleChange}
+          value="sandals"
+          title="Sandals"
+          name="category"
+        />
+        <Input
+          handleChange={handleChange}
+          value="heels"
+          title="Heels"
+          name="category"
+        />
       </div>
     </div>
   );
