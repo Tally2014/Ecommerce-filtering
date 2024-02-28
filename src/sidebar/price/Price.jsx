63160bbd705ada5f8144eba203789ed2ce500a5a
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 22:28:49 +0200

    01:27:37 - Price Component Refactor

--- src/sidebar/price/Price.jsx
+++ src/sidebar/price/Price.jsx
@@ -1,20 +1,39 @@
 import "./Price.css";
 import Input from "../../components/Input";
 
-export default function Price() {
+export default function Price({ handleChange }) {
   return (
     <>
       <div className="ml">
         <h2 className="sidebar-title price-title">Price</h2>
-        <label className="sidebar-label-container">
-          <input type="radio" value="" name="test2" />
+        <label onChange={handleChange} className="sidebar-label-container">
+          <input type="radio" value="" name="price" />
           <span className="checkmark"></span>All
         </label>
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
+        <Input
+          handleChange={handleChange}
+          value={50}
+          title="$0 - 50"
+          name="price"
+        />
+        <Input
+          handleChange={handleChange}
+          value={100}
+          title="$50 - 100"
+          name="price"
+        />
+        <Input
+          handleChange={handleChange}
+          value={150}
+          title="$100 - 150"
+          name="price"
+        />
+        <Input
+          handleChange={handleChange}
+          value={200}
+          title="Over $200"
+          name="price"
+        />
       </div>
     </>
   );
