227e41ecc0413124a53ac01103853fe0fdb23a35
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 18:24:40 +0200

    50:05 - Price Section

--- src/sidebar/price/Price.jsx
+++ src/sidebar/price/Price.jsx
@@ -2,6 +2,30 @@ import "./Price.css";
 
 export default function Price() {
   return (
-    <div>Price</div>
-  )
+    <>
+      <div className="ml">
+        <h2 className="sidebar-title price-title">Price</h2>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>All
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>R0 - R1000
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>R1000 - R3000
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span>R3000 - R5000
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" value="" name="test2" />
+          <span className="checkmark"></span> Over R5000
+        </label>
+      </div>
+    </>
+  );
 }
