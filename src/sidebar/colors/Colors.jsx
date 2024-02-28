f7e5a4e716625e4ae0b0cdfb64aa6af07585b7ba
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 23:03:23 +0200

    01:29:33 - Color Component Refactor

--- src/sidebar/colors/Colors.jsx
+++ src/sidebar/colors/Colors.jsx
@@ -1,23 +1,51 @@
 import "./Colors.css";
 import Input from "../../components/Input";
 
-export default function Colors() {
+export default function Colors({ handleChange }) {
   return (
     <>
       <div className="ml">
         <h2 className="sidebar-title price-title">Colors</h2>
-        <label className="sidebar-label-container">
-          <input type="radio" name="test3" />
-          <span className="checkmark"></span>All
+        <label onChange={handleChange} className="sidebar-label-container">
+          <input type="radio" name="colors" />
+          <span className="checkmark all"></span>All
         </label>
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
+        <Input
+          onChange={handleChange}
+          value="black"
+          title="Black"
+          name="colors"
+          color="black"
+        />
+        <Input
+          onChange={handleChange}
+          value="blue"
+          title="Blue"
+          name="colors"
+          color="blue"
+        />
+        <Input
+          onChange={handleChange}
+          value="red"
+          title="Red"
+          name="colors"
+          color="red"
+        />
+        <Input
+          onChange={handleChange}
+          value="green"
+          title="Green"
+          name="colors"
+          color="green"
+        />
+        <label onChange={handleChange} className="sidebar-label-container">
+          <input type="radio" name="colors" value="white" />
+          <span
+            className="checkmark"
+            style={{ background: "white", border: "2px solid black" }}
+          ></span>
+          White
+        </label>
       </div>
     </>
   );
