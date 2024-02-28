1b035d109a2b5389a4baee940e8e84fe124daef2
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 18:50:19 +0200

    Fix Price Section Radio Button ot be enabled

--- src/sidebar/price/Price.jsx
+++ src/sidebar/price/Price.jsx
@@ -5,23 +5,23 @@ export default function Price() {
     <>
       <div className="ml">
         <h2 className="sidebar-title price-title">Price</h2>
-        <label htmlFor="" className="sidebar-label-container">
+        <label className="sidebar-label-container">
           <input type="radio" value="" name="test2" />
           <span className="checkmark"></span>All
         </label>
-        <label htmlFor="" className="sidebar-label-container">
+        <label className="sidebar-label-container">
           <input type="radio" value="" name="test2" />
           <span className="checkmark"></span>R0 - R1000
         </label>
-        <label htmlFor="" className="sidebar-label-container">
+        <label className="sidebar-label-container">
           <input type="radio" value="" name="test2" />
           <span className="checkmark"></span>R1000 - R3000
         </label>
-        <label htmlFor="" className="sidebar-label-container">
+        <label className="sidebar-label-container">
           <input type="radio" value="" name="test2" />
           <span className="checkmark"></span>R3000 - R5000
         </label>
-        <label htmlFor="" className="sidebar-label-container">
+        <label className="sidebar-label-container">
           <input type="radio" value="" name="test2" />
           <span className="checkmark"></span> Over R5000
         </label>
