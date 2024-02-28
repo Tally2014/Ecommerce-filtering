e175e8c0a1f18aacd96f73c390be9853b8f94b53
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 21:54:07 +0200

    01:06:51 - Buttons Filter Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Products from "./products/Products";
 import Recommended from "./recommended/Recommended";
 import Sidebar from "./sidebar/Sidebar";
 import products from "./db/data";
+import Card from "./components/Card";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -26,6 +27,47 @@ function App() {
   const handleChange = (e) => {
     setSelectedCategory(e.target.value);
   };
+
+  // ------------ Button Filtering -----------
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    //Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={crypto.randomUUID()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  //const result =filteredData(products, selectedCategory, query);
 
   return (
     <>
