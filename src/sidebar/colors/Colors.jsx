86403c9a6ea0c25e783348c7e0adfd8b989565ee
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 18:48:18 +0200

    52:10 - Color Section

--- src/sidebar/colors/Colors.jsx
+++ src/sidebar/colors/Colors.jsx
@@ -1,7 +1,35 @@
-import "./Colors.css"
+import "./Colors.css";
 
 export default function Colors() {
   return (
-    <div>Colors</div>
-  )
+    <>
+      <div className="ml">
+        <h2 className="sidebar-title price-title">Colors</h2>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>black
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>blue
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span>red
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span> green
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test3" />
+          <span className="checkmark"></span> white
+        </label>
+      </div>
+    </>
+  );
 }
