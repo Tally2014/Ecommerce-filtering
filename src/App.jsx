75e201f11487330cdf6f56ea792576be71645eb3
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 19:28:54 +0200

    01:02:05 - Input Filter Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 import Navigation from "./navigation/Nav";
 import Products from "./products/Products";
 import Recommended from "./recommended/Recommended";
 import Sidebar from "./sidebar/Sidebar";
+import products from "./db/data";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  //Input Filter
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (p) => p.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1
+  );
+
+  console.log(filteredItems);
+
   return (
     <>
       <Sidebar />
