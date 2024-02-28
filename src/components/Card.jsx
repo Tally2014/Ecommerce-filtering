93db6c6e2573f302fedcce5cdcae4b4049f62987
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-28 19:15:46 +0200

    53:50 - Refactor To Clean Code (Card Component)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,38 @@
-export default function Card() {
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
+
+export default function Card({
+  img,
+  title,
+  star,
+  reviews,
+  prevPrice,
+  newPrice,
+}) {
   return (
-    <div>Card</div>
-  )
+    <>
+      <section className="card">
+        <img src={img} alt={title} className="card-img" />
+        <div className="card-details">
+          <h3 className="card-title">{title}</h3>
+          <section className="card-reviews">
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <span className="total-reviews">{reviews}</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>{prevPrice}</del> {newPrice}
+            </div>
+
+            <div className="bag">
+              <BsFillBagHeartFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
 }
